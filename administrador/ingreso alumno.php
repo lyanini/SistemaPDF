b38ceb32f60992ceff5e39a4504c9276b2a8d52a
morningstar123 <753951852456>
2017-11-20 06:03:45 -0300

    adminstrador

---
+++ administrador/ingreso alumno.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+  </head>
+<body>
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet"  href="estilos.css">
+<form role="form">
+  <div class="form-group">
+    <label for="ejemplo_email_1">Email</label>
+    <input type="email" class="form-control" id="ejemplo_email_1"
+           placeholder="Introduce tu email">
+  </div>
+  <div class="form-group">
+    <label for="ejemplo_password_1">Contraseña</label>
+    <input type="password" class="form-control" id="ejemplo_password_1" 
+           placeholder="Contraseña">
+  </div>
+  <div class="form-group">
+    <label for="ejemplo_archivo_1">Adjuntar un archivo</label>
+    <input type="file" id="ejemplo_archivo_1">
+    <p class="help-block">Ejemplo de texto de ayuda.</p>
+  </div>
+  <div class="checkbox">
+    <label>
+      <input type="checkbox"> Activa esta casilla
+    </label>
+  </div>
+  <button type="submit" class="btn btn-default">Enviar</button>
+</form>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
